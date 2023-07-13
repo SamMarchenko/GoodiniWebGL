e25f51e8177224e609f8124e2065d6eae27f8ebc
Sam Marchenko <sam.marchenko@gmail.com>
2023-07-13 20:52:18 +0300

    Обновил плагин leanTouch и добавил вращение камеры вокруг квартиры

---
+++ Assets/Scripts/CameraMoving.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class CameraMoving : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 _previousPosition;
+    private Camera _camera;
+
+    void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    void LateUpdate()
+    {
+        #region PC
+
+        if (Input.GetMouseButtonDown(0)) 
+            _previousPosition = _camera.ScreenToViewportPoint(Input.mousePosition);
+
+        #endregion
+
+        #region Mobile
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            _previousPosition = _camera.ScreenToViewportPoint(Input.GetTouch(0).position);
+
+        if (Input.GetMouseButton(0))
+            MoveCamera(Input.mousePosition);
+
+        else if (Input.touchCount == 1) 
+            MoveCamera(Input.touches[0].position);
+
+        #endregion
+    }
+
+    private void MoveCamera(Vector3 touchPos)
+    {
+        Vector3 direction = _previousPosition - _camera.ScreenToViewportPoint(touchPos);
+
+        _camera.transform.position = target.position;
+
+        _camera.transform.Rotate(new Vector3(1, 0, 0), direction.y * 180);
+        _camera.transform.Rotate(new Vector3(0, 1, 0), -direction.x * 180, Space.World);
+        _camera.transform.Translate(new Vector3(0, 0, -10));
+
+        _previousPosition = _camera.ScreenToViewportPoint(touchPos);
+    }
+}
